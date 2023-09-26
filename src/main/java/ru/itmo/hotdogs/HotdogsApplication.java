16866774fe6d05fa935e12f70450898da41e9dbe
R4MZEZ <kazachenko2003@bk.ru>
2023-09-26 20:04:44 +0300

    generated skeleton from start.spring.io

---
+++ src/main/java/ru/itmo/hotdogs/HotdogsApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.hotdogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotdogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotdogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/itmo/hotdogs/HotdogsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.hotdogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotdogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
