94f693792db2314689368301ab8b27a8b9a17a4e
R4MZEZ <kazachenko2003@bk.ru>
2023-10-07 23:17:14 +0300

    feat: added BreedService

---
+++ src/main/java/ru/itmo/hotdogs/service/BreedService.java
@@ -0,0 +1,19 @@
+package ru.itmo.hotdogs.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.itmo.hotdogs.model.BreedEntity;
+import ru.itmo.hotdogs.repository.BreedRepository;
+
+@Service
+@RequiredArgsConstructor
+public class BreedService {
+
+
+  private final BreedRepository breedRepository;
+
+  public List<BreedEntity> findAll() {
+    return breedRepository.findAll();
+  }
+}
