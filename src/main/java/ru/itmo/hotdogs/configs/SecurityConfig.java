b4a967f6a0db033cc1c5021dc1a15dbecebd0868
R4MZEZ <kazachenko2003@bk.ru>
2023-10-20 19:48:49 +0300

    feat(auth): added security configuration
    Добавлена конфигурация слоя безопасности с указанием метода кодирования пароля и фильтров на маппинги

---
+++ src/main/java/ru/itmo/hotdogs/configs/SecurityConfig.java
@@ -0,0 +1,68 @@
+package ru.itmo.hotdogs.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import ru.itmo.hotdogs.service.UserService;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
+@Configuration
+public class SecurityConfig {
+
+	private final UserService userService;
+
+
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		daoAuthenticationProvider.setUserDetailsService(userService);
+		return daoAuthenticationProvider;
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/owners/**").hasRole("OWNER")
+				.requestMatchers("/dogs/**").hasRole("DOG")
+				.anyRequest().permitAll())
+			.sessionManagement(
+				session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.exceptionHandling(
+				exception -> exception.authenticationEntryPoint(new HttpStatusEntryPoint(
+					HttpStatus.UNAUTHORIZED))
+			)
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(AbstractHttpConfigurer::disable);
+
+		return http.build();
+	}
+
+
+
+}
