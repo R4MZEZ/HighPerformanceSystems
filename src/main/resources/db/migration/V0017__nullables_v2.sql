583c74a3d8fadd65e67f11fdb985e7bdd53ca5ad
R4MZEZ <kazachenko2003@bk.ru>
2023-10-24 00:47:51 +0300

    feat(sql): new migrations

---
+++ src/main/resources/db/migration/V0017__nullables_v2.sql
@@ -0,0 +1,2 @@
+alter table dogs_interests alter column level drop not null;
+alter table dogs_interests alter column level set default 1;
---
+++ src/main/resources/db/migration/V0018__admin_user.sql
@@ -0,0 +1,2 @@
+insert into users (login, password) values ('admin', '$2a$12$82xsyEJxQEJH9WNx1tMp7u3AfstT1AP.WytFVRaOOoIw1RQ.gpbc6');
+insert into users_roles (user_id, role_id) values (1, 3);
