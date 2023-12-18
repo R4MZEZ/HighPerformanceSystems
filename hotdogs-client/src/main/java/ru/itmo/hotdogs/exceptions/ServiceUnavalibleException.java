08c8b2b099b4b8a9f6b83868a6bad5ec29b151e7
R4MZEZ <kazachenko2003@bk.ru>
2023-12-18 23:54:26 +0300

    feat(dto): added DTO for feign exceptions

---
+++ hotdogs-client/src/main/java/ru/itmo/hotdogs/exceptions/ServiceUnavalibleException.java
@@ -0,0 +1,8 @@
+package ru.itmo.hotdogs.exceptions;
+
+public class ServiceUnavalibleException extends Exception{
+
+	public ServiceUnavalibleException(String message) {
+		super(message);
+	}
+}
---
+++ owner-service/src/main/java/ru/itmo/ownerservice/model/dto/ResponseDto.java
@@ -0,0 +1,8 @@
+package ru.itmo.ownerservice.model.dto;
+
+import java.io.Serializable;
+import org.springframework.http.HttpStatus;
+
+
+public record ResponseDto<T>(T body, Throwable error, HttpStatus code) implements Serializable {
+}
