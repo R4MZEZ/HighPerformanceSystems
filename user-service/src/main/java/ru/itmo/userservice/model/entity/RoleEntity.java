5b61f905be8173537f61e7bdc9c46bb063f93035
R4MZEZ <kazachenko2003@bk.ru>
2023-12-22 14:52:06 +0300

    feat(user-service): removed jpa annotations from reactive service

--- user-service/src/main/java/ru/itmo/userservice/model/entity/RoleEntity.java
+++ user-service/src/main/java/ru/itmo/userservice/model/entity/RoleEntity.java
@@ -1,60 +1,47 @@
 package ru.itmo.userservice.model.entity;
 
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-import java.util.Objects;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-//import org.hibernate.proxy.HibernateProxy;
-
-//@Entity(name = "roles")
 @Getter
 @Setter
 @NoArgsConstructor
-//@Table(name = "roles")
 public class RoleEntity {
 
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	@Column(name = "id", nullable = false)
 	private Long id;
 
-//	@Column
 	@NotBlank
 	@Pattern(regexp = "^[a-zA-Z_]+$")
 	private String name;
-
 
 	public RoleEntity(String name) {
 		this.name = name;
 	}
 
 	@Override
-	public final boolean equals(Object o) {
+	public boolean equals(Object o) {
 		if (this == o) {
 			return true;
 		}
-		if (o == null) {
+		if (o == null || getClass() != o.getClass()) {
 			return false;
 		}
-//		Class<?> oEffectiveClass = o instanceof HibernateProxy
-//			? ((HibernateProxy) o).getHibernateLazyInitializer()
-//			.getPersistentClass() : o.getClass();
-//		Class<?> thisEffectiveClass = this instanceof HibernateProxy
-//			? ((HibernateProxy) this).getHibernateLazyInitializer()
-//			.getPersistentClass() : this.getClass();
-//		if (thisEffectiveClass != oEffectiveClass) {
-//			return false;
-//		}
-		RoleEntity role = (RoleEntity) o;
-		return getId() != null && Objects.equals(getId(), role.getId());
+
+		RoleEntity that = (RoleEntity) o;
+
+		if (!id.equals(that.id)) {
+			return false;
+		}
+		return name.equals(that.name);
 	}
 
+	@Override
+	public int hashCode() {
+		int result = id.hashCode();
+		result = 31 * result + name.hashCode();
+		return result;
+	}
 }
